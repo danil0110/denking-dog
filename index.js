e14f77a4db4c08c790187ab19ee8d8c1c0e89439
danil0110 <danilmail0110@gmail.com>
2021-06-04 21:07:14 +0300

    Added simple message responses

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const TelegramApi = require('node-telegram-bot-api');
+const triggerMessages = require('./trigger-messages');
+
+const token = process.env.TG_TOKEN;
+const bot = new TelegramApi(token, { polling: true });
+
+bot.on('message', msg => {
+	const text = msg.text;
+	const chatId = msg.chat.id;
+	const firstName = msg.from.first_name;
+	const lastName = msg.from.last_name;
+
+	console.log(text.toLowerCase());
+
+	if (text === '/start') {
+		bot.sendMessage(chatId, `Привет, ${firstName} ${lastName}! Я буду твоим самым верным псом!`);
+	}
+
+	if (text === '/info') {
+		bot.sendMessage(chatId, `Хочешь узнать меня поближе? Я самый дружелюбный пес, который просто хочет человеческого внимания! Будешь моим хозяином?`);
+	}
+
+	if (triggerMessages.includes(text.toLowerCase())) {
+		bot.sendMessage(chatId, 'Гав! Слушаюсь!');
+	}
+});
